3fdf09a9842c667226bde9dc6322a1d2ab41f182
Aodhgan <aodhgan@pooltogether.com>
2021-06-03 11:33:07 -0700

    removed transfer from interface

--- contracts/ISushiBar.sol
+++ contracts/ISushiBar.sol
@@ -11,7 +11,4 @@ interface ISushiBar {
 
     function balanceOf(address account) external view returns (uint256);
 
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
 }
